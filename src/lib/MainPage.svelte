125d73129bb747a7e79694bc36d819829fafd32e
benedikt <benedikt@isg.cs.ovgu.de>
2025-02-06 12:02:01 +0100

    init

---
+++ src/lib/MainPage.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { i18n } from '$lib/i18n';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import * as m from '$lib/paraglide/messages.js';
+
+	/**
+	 * @param {import("$lib/paraglide/runtime").AvailableLanguageTag} newLanguage
+	 */
+	function switchToLanguage(newLanguage) {
+		const canonicalPath = i18n.route(page.url.pathname);
+		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+		goto(localisedPath);
+	}
+</script>
+
+<div class="desktop-nav to-fade-in">
+	<div class="center-container">
+		<nav class="heading-nav slim-fit-container space-between-container">
+			<div>
+				<a href="https://benediktmayer.com/" class="logo-link"
+					><span class="logo-text-content">Benedikt Mayer</span></a
+				>
+			</div>
+			<div>
+				<button onclick={() => switchToLanguage('en')}>en</button>
+				<button onclick={() => switchToLanguage('de')}>de</button>
+			</div>
+			<div>
+				<ul class="nav-links gap-row-container">
+					<li>
+						<a href="#projects-title" class="page-link"><span>{m.title_projects_abbrev()}</span></a>
+					</li>
+					<li><a href="#about-title" class="page-link"><span>{m.title_about()}</span></a></li>
+					<li><a href="#contact-title" class="page-link"><span>{m.title_contact()}</span></a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../styles.css';
+
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
