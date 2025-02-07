e53a360317b721e6c57bf403c70c73578f3a538a
benedikt <benedikt@isg.cs.ovgu.de>
2025-02-07 10:37:32 +0100

    added project page styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../styles.css';
+	import '../mediaqueries.css';
 
 	let { children } = $props();
 </script>
--- src/routes/projects/characterization/+layout.svelte
+++ src/routes/projects/characterization/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../../../project_page_styles.css';
+	import '../../../project_page_mediaqueries.css';
 
 	let { children } = $props();
 </script>
